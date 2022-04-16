e6abe564429402ee70f8370da43153ca97204137
Andrew Chisholm <awchisholm@gmail.com>
2022-04-17 00:29:55 +0100

    tidy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ministerial_directions
 
-Smoe data and an analysis showing that the number of ministerial directions in the UK is going up. This only shows up if you normalise the number of such directions by the time in office of the prime minister.
+Some data and an analysis showing that the number of ministerial directions in the UK is going up. This only shows up if you normalise the number of such directions by the time in office of the prime minister.
 
 The graph looks like this 
 
--- ministerial_directions.R
+++ ministerial_directions.R
@@ -3,21 +3,21 @@
 library(lubridate)
 library(dplyr)
 library(ggplot2)
-pm = read.csv('directions.csv',strip.white = T, check.names = F)
-pm$Start=dmy(pm$Start)
-pm$End=dmy(pm$End)
+pm <- read.csv('directions.csv', strip.white = T, check.names = F)
+pm$Start <- dmy(pm$Start)
+pm$End <- dmy(pm$End)
 
-pm$Duration=as.numeric(pm$End-pm$Start)
-pm$DirectionsPerYear = 365*pm$`Number of ministerial directions`/pm$Duration
+pm$Duration <- as.numeric(pm$End-pm$Start)
+pm$DirectionsPerYear <- 365*pm$`Number of ministerial directions`/pm$Duration
 
-pm$`Prime Minister` = factor(pm$`Prime Minister`, levels = pm$`Prime Minister`)
-gg=ggplot(data=pm)
-ggbar=geom_col(mapping=aes(x=`Prime Minister`, y=DirectionsPerYear, fill=`Prime Minister`))
+pm$`Prime Minister` <- factor(pm$`Prime Minister`, levels = pm$`Prime Minister`)
+gg <- ggplot(data=pm)
+ggbar <- geom_col(mapping=aes(x=`Prime Minister`, y=DirectionsPerYear, fill=`Prime Minister`))
 
-gglabelsx = xlab('Prime Minister')
-gglabelsy = ylab('Ministerial Directions Per Year')
-ggtitles = ggtitle('Ministerial Directions Per Year For Recent UK Prime Ministers')
-ggt = theme(text = element_text(size=20), axis.text.x = element_text(angle=90, hjust=1)) 
+gglabelsx <- xlab('Prime Minister')
+gglabelsy <- ylab('Ministerial Directions Per Year')
+ggtitles <- ggtitle('Ministerial Directions Per Year For Recent UK Prime Ministers')
+ggt <- theme(text = element_text(size=20), axis.text.x = element_text(angle=90, hjust=1)) 
 
 gg+ggbar+gglabelsx+gglabelsy+ggtitles+ggt
 
