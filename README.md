89aebaaad4734215ffe9595a8d11b87ebada749b
Andrew Chisholm <awchisholm@gmail.com>
2022-04-17 00:25:27 +0100

    save

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ministerial_directions
 
 Smoe data and an analysis showing that the number of ministerial directions in the UK is going up. This only shows up if you normalise the number of such directions by the time in office of the prime minister.
+
+The graph looks like this 
+
+![Ministerial Directions per year for recent UK prime ministers][ministerial_directions.png]
---
+++ ministerial_directions.R
@@ -0,0 +1,23 @@
+#https://www.instituteforgovernment.org.uk/charts/ministerial-directions-department-over-time
+
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+pm = read.csv('directions.csv',strip.white = T, check.names = F)
+pm$Start=dmy(pm$Start)
+pm$End=dmy(pm$End)
+
+pm$Duration=as.numeric(pm$End-pm$Start)
+pm$DirectionsPerYear = 365*pm$`Number of ministerial directions`/pm$Duration
+
+pm$`Prime Minister` = factor(pm$`Prime Minister`, levels = pm$`Prime Minister`)
+gg=ggplot(data=pm)
+ggbar=geom_col(mapping=aes(x=`Prime Minister`, y=DirectionsPerYear, fill=`Prime Minister`))
+
+gglabelsx = xlab('Prime Minister')
+gglabelsy = ylab('Ministerial Directions Per Year')
+ggtitles = ggtitle('Ministerial Directions Per Year For Recent UK Prime Ministers')
+ggt = theme(text = element_text(size=20), axis.text.x = element_text(angle=90, hjust=1)) 
+
+gg+ggbar+gglabelsx+gglabelsy+ggtitles+ggt
+
